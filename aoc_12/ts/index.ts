321842040d481fc5b1f5a9a029e2bbb59c53b834
Taylor Sweetman <taylor.sweetman@glia.com>
2022-12-13 23:31:39 -0500

    12 part 2 soln

--- aoc_12/ts/index.ts
+++ aoc_12/ts/index.ts
@@ -11,8 +11,13 @@ type Node = {
 type State = {
   currentNode: Node;
   unvisited: Record<string, Node>;
-  destIdx: string;
+  endCondition: "idx" | "height";
+  endConditionValue: string;
 };
+
+type VisitorFunc = (current: Node, toVisit: Node) => boolean;
+
+const INF = Number.MAX_SAFE_INTEGER;
 
 const getUpIdx = (idx: string) =>
   idx.split("|").reduce((acc, current, i) => {
@@ -38,67 +43,84 @@ const getLeftIdx = (idx: string) =>
     return `${acc}|${parseInt(current) - 1}`;
   }, "");
 
-const canVisit = (current: Node, toVisit: Node) =>
+const canVisitOneHigher = (current: Node, toVisit: Node) =>
   toVisit.height.charCodeAt(0) - current.height.charCodeAt(0) <= 1;
 
-const findTraversableNs = (
-  current: Node,
-  nodeMap: Record<string, Node>
-): string[] => {
-  const result: string[] = [];
+const canVisitOneLower = (current: Node, toVisit: Node) =>
+  current.height.charCodeAt(0) - toVisit.height.charCodeAt(0) <= 1;
 
-  const upNode = nodeMap[getUpIdx(current.idx)];
-  const downNode = nodeMap[getDownIdx(current.idx)];
-  const rightNode = nodeMap[getRightIdx(current.idx)];
-  const leftNode = nodeMap[getLeftIdx(current.idx)];
+const findTraversableNs =
+  (visitor: VisitorFunc) =>
+  (current: Node, nodeMap: Record<string, Node>): string[] => {
+    const result: string[] = [];
 
-  if (upNode && canVisit(current, upNode)) result.push(upNode.idx);
-  if (downNode && canVisit(current, downNode)) result.push(downNode.idx);
-  if (rightNode && canVisit(current, rightNode)) result.push(rightNode.idx);
-  if (leftNode && canVisit(current, leftNode)) result.push(leftNode.idx);
+    const upNode = nodeMap[getUpIdx(current.idx)];
+    const downNode = nodeMap[getDownIdx(current.idx)];
+    const rightNode = nodeMap[getRightIdx(current.idx)];
+    const leftNode = nodeMap[getLeftIdx(current.idx)];
 
-  return result;
-};
+    if (upNode && visitor(current, upNode)) result.push(upNode.idx);
+    if (downNode && visitor(current, downNode)) result.push(downNode.idx);
+    if (rightNode && visitor(current, rightNode)) result.push(rightNode.idx);
+    if (leftNode && visitor(current, leftNode)) result.push(leftNode.idx);
 
-const parse = (input: string): State => {
-  const nodeMap: Record<string, Node> = {};
-  let currentIdx = "";
-  let destIdx = "";
-
-  const lines = input.split("\n");
-  lines.forEach((line, xIdx) =>
-    line.split("").forEach((char, yIdx) => {
-      const nodeIdx = `${xIdx}|${yIdx}`;
-      const start = char === "S";
-      const dest = char === "E";
-
-      const height = start ? "a" : dest ? "z" : char;
-      const distanceFromStart = start ? 0 : Number.MAX_SAFE_INTEGER;
-      const node: Node = {
-        height,
-        distanceFromStart,
-        idx: nodeIdx,
-        traversableNs: [],
-      };
-
-      nodeMap[nodeIdx] = node;
-      if (start) currentIdx = nodeIdx;
-      if (dest) destIdx = nodeIdx;
-    })
-  );
-
-  const finalMap = Object.entries(nodeMap).reduce((acc, [idx, node]) => {
-    const traversableNs = findTraversableNs(node, nodeMap);
-    const updatedNode = { ...node, traversableNs };
-    return { ...acc, [idx]: updatedNode };
-  }, {} as Record<string, Node>);
-
-  return {
-    unvisited: finalMap,
-    currentNode: finalMap[currentIdx],
-    destIdx,
+    return result;
   };
-};
+
+const parse =
+  (visitor: VisitorFunc, endCondition: "idx" | "height") =>
+  (input: string): State => {
+    const nodeMap: Record<string, Node> = {};
+    let currentIdx = "";
+    let destIdx = "";
+
+    const lines = input.split("\n");
+    lines.forEach((line, xIdx) =>
+      line.split("").forEach((char, yIdx) => {
+        const nodeIdx = `${xIdx}|${yIdx}`;
+        const start =
+          (endCondition === "idx" && char === "S") ||
+          (endCondition === "height" && char === "E");
+        const dest = endCondition === "idx" && char === "E";
+
+        let height = "";
+        if (start && endCondition === "idx") {
+          height = "a";
+        } else if (start && endCondition) {
+          height = "z";
+        } else if (dest && endCondition === "idx") {
+          height = "z";
+        } else {
+          height = char;
+        }
+
+        const distanceFromStart = start ? 0 : INF;
+        const node: Node = {
+          height,
+          distanceFromStart,
+          idx: nodeIdx,
+          traversableNs: [],
+        };
+
+        nodeMap[nodeIdx] = node;
+        if (start) currentIdx = nodeIdx;
+        if (dest) destIdx = nodeIdx;
+      })
+    );
+
+    const finalMap = Object.entries(nodeMap).reduce((acc, [idx, node]) => {
+      const traversableNs = findTraversableNs(visitor)(node, nodeMap);
+      const updatedNode = { ...node, traversableNs };
+      return { ...acc, [idx]: updatedNode };
+    }, {} as Record<string, Node>);
+
+    return {
+      unvisited: finalMap,
+      currentNode: finalMap[currentIdx],
+      endCondition,
+      endConditionValue: endCondition === "idx" ? destIdx : "a",
+    };
+  };
 
 const findNearestUnvisited = (unvisited: Record<string, Node>): Node =>
   (_.minBy(
@@ -106,17 +128,30 @@ const findNearestUnvisited = (unvisited: Record<string, Node>): Node =>
     ([_, node]) => node.distanceFromStart
   ) ?? ["", {} as Node])[1];
 
-const runPathfinder = (currentState: State): number => {
-  const { destIdx, unvisited } = currentState;
+const findShortestPath = (currentState: State): number => {
+  const { endCondition, endConditionValue, unvisited } = currentState;
 
-  while (destIdx in unvisited) {
+  let shortest = INF;
+
+  while (Object.keys(unvisited).length > 1) {
     const { currentNode } = currentState;
-    if (currentNode.idx === destIdx) return currentNode.distanceFromStart;
 
+    // base case, we've found the shortest path to the destination
+    if (endCondition === "idx" && currentNode.idx === endConditionValue)
+      return currentNode.distanceFromStart;
+
+    if (
+      endCondition === "height" &&
+      currentNode.height === endConditionValue &&
+      currentNode.distanceFromStart < shortest
+    ) {
+      shortest = currentNode.distanceFromStart;
+    }
+
+    // find adjacent, unvisited nodes. mark current node as visited
     const nodesToCompare = currentNode.traversableNs
       .filter((nodeIdx) => nodeIdx in unvisited)
       .map((nodeIdx) => unvisited[nodeIdx]);
-
     delete currentState.unvisited[currentNode.idx];
 
     // no traversable nodes which have not been visited already
@@ -139,7 +174,7 @@ const runPathfinder = (currentState: State): number => {
       },
       { distanceFromStart: newDist } as Node
     );
-    if (closestNewNode.distanceFromStart === Number.MAX_SAFE_INTEGER) {
+    if (closestNewNode.distanceFromStart === INF) {
       currentState.currentNode = findNearestUnvisited(currentState.unvisited);
       continue;
     }
@@ -149,13 +184,28 @@ const runPathfinder = (currentState: State): number => {
     });
     currentState.currentNode = closestNewNode;
   }
-  return -1;
+
+  return shortest || INF;
 };
 
 const main = () => {
-  let currentState = parse(readInput(__dirname));
-  const dist = runPathfinder(currentState);
+  // Part 1
+  const partOneStatingState = parse(
+    canVisitOneHigher,
+    "idx"
+  )(readInput(__dirname));
+
+  const dist = findShortestPath(partOneStatingState);
   logAndAssert(dist, 423);
+
+  // Part 2
+  const partTwoStatingState = parse(
+    canVisitOneLower,
+    "height"
+  )(readInput(__dirname));
+
+  const distTwo = findShortestPath(partTwoStatingState);
+  logAndAssert(distTwo, 416);
 };
 
 main();
