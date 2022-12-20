4e12c224508803e3e7b218270913efed7ffcc535
Taylor Sweetman <taylor.sweetman@glia.com>
2022-12-20 09:38:04 -0500

    15 part 1

--- aoc_15/ts/index.ts
+++ aoc_15/ts/index.ts
@@ -1,15 +1,16 @@
-import { readInput } from "../../ts_lib";
+import _ from "lodash";
+import { logAndAssert, readInput } from "../../ts_lib";
 
 type SensorInfo = {
-  sensorLoc: [number, number];
-  beaconLoc: [number, number];
+  sensorLoc: Coord;
+  beaconLoc: Coord;
   mDist: number;
 };
-
-type CoordPair = [[number, number], [number, number]];
+type Coord = { x: number; y: number };
+type CoordPair = [Coord, Coord];
 
 const findMDist = (pair: CoordPair): number =>
-  Math.abs(pair[0][0] - pair[1][0]) + Math.abs(pair[0][1] - pair[1][1]);
+  Math.abs(pair[0].x - pair[1].x) + Math.abs(pair[0].y - pair[1].y);
 
 const parse = (input: string): SensorInfo[] => {
   const lines = input.split("\n");
@@ -19,17 +20,20 @@ const parse = (input: string): SensorInfo[] => {
         const matches = text.match(/[0-9-]+/g) ?? [];
         if (idx === 0)
           return [
-            [parseInt(matches[0] ?? "-1"), parseInt(matches[1] ?? "-1")],
-            [-1, -1],
+            {
+              x: parseInt(matches[0] ?? "-1"),
+              y: parseInt(matches[1] ?? "-1"),
+            },
+            { x: -1, y: -1 },
           ];
         return [
           acc[0],
-          [parseInt(matches[0] ?? "-1"), parseInt(matches[1] ?? "-1")],
+          { x: parseInt(matches[0] ?? "-1"), y: parseInt(matches[1] ?? "-1") },
         ];
       },
       [
-        [-1, -1],
-        [-1, -1],
+        { x: -1, y: -1 },
+        { x: -1, y: -1 },
       ]
     )
   );
@@ -41,9 +45,37 @@ const parse = (input: string): SensorInfo[] => {
   }));
 };
 
+const findRowTouches =
+  (yIdx: number) =>
+  (sensor: SensorInfo): number[] => {
+    const { sensorLoc, mDist } = sensor;
+    const { x: sensorX, y: sensorY } = sensorLoc;
+
+    const distanceFromRow = Math.abs(yIdx - sensorY);
+    const leftOver = mDist - distanceFromRow;
+    if (leftOver < 0) return [];
+
+    const min = sensorX - leftOver;
+    const max = sensorX + leftOver;
+    return _.range(min, max + 1);
+  };
+
 const main = () => {
   const sensors = parse(readInput(__dirname));
-  console.log(sensors);
+  const y = 2_000_000;
+  const findTouchesOfRow = findRowTouches(y);
+
+  const uniqTouches = new Set(
+    sensors.flatMap((sensor) => findTouchesOfRow(sensor))
+  );
+  const beaconTouches = new Set(
+    sensors
+      .filter(({ beaconLoc }) => beaconLoc.y === y)
+      .map(({ beaconLoc }) => beaconLoc.x)
+  );
+
+  const noBeacon = [...uniqTouches].filter((y) => !beaconTouches.has(y));
+  logAndAssert(noBeacon.length, 5_040_643);
 };
 
 main();
