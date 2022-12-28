2ac632a292bdd98ca926606fc53c93cc4ce50718
Taylor Sweetman <taylor.sweetman@gmail.com>
2022-12-28 17:11:24 -0500

    Coord class

--- ts_lib/index.ts
+++ ts_lib/index.ts
@@ -17,3 +17,66 @@ export const isUpperCase = (char: string) => char === char.toUpperCase();
 
 export const readInput = (dirname: string) =>
   readFileSync(resolve(dirname, "../input.txt"), "utf8");
+
+export class Coord {
+  id: string;
+  arr: number[];
+  x: number;
+  y: number;
+  z: number;
+
+  constructor(coordStr: string) {
+    this.id = coordStr;
+    this.arr = this.id.split(",").map((num) => parseInt(num));
+    this.x = this.arr[0];
+    this.y = this.arr[1];
+    this.z = this.arr[2];
+  }
+
+  adjacentCords() {
+    return [
+      new Coord(`${this.x - 1},${this.y},${this.z}`),
+      new Coord(`${this.x + 1},${this.y},${this.z}`),
+      new Coord(`${this.x},${this.y - 1},${this.z}`),
+      new Coord(`${this.x},${this.y + 1},${this.z}`),
+      new Coord(`${this.x},${this.y},${this.z - 1}`),
+      new Coord(`${this.x},${this.y},${this.z + 1}`),
+    ];
+  }
+
+  isAdjecentTo(coord: Coord) {
+    return this.adjacentCords().some((c) => c.id === coord.id);
+  }
+
+  xMinusOne() {
+    return new Coord(`${this.x - 1},${this.y},${this.z}`);
+  }
+
+  xPlusOne() {
+    return new Coord(`${this.x + 1},${this.y},${this.z}`);
+  }
+
+  yMinusOne() {
+    return new Coord(`${this.x},${this.y - 1},${this.z}`);
+  }
+
+  yPlusOne() {
+    return new Coord(`${this.x},${this.y + 1},${this.z}`);
+  }
+
+  zMinusOne() {
+    return new Coord(`${this.x},${this.y},${this.z - 1}`);
+  }
+
+  zPlusOne() {
+    return new Coord(`${this.x},${this.y},${this.z + 1}`);
+  }
+
+  isPastMin(min: Coord) {
+    return this.x < min.x || this.y < min.y || this.z < min.z;
+  }
+
+  isPastMax(max: Coord) {
+    return this.x > max.x || this.y > max.y || this.z > max.z;
+  }
+}
