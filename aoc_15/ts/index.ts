3c83cc0b02362588236252a5883213e408994e6e
Taylor Sweetman <taylor.sweetman@glia.com>
2022-12-29 13:36:27 -0500

    15 refactor and part 2 soln

--- aoc_15/ts/index.ts
+++ aoc_15/ts/index.ts
@@ -1,9 +1,9 @@
 import {
-  containedCount,
   logAndAssert,
   mergeAll,
   readInput,
   Coord,
+  CoordPair,
 } from "../../ts_lib";
 
 type SensorInfo = {
@@ -11,7 +11,6 @@ type SensorInfo = {
   beaconLoc: Coord;
   mDist: number;
 };
-type CoordPair = [Coord, Coord];
 
 const findMDist = (pair: CoordPair): number =>
   Math.abs(pair[0].x - pair[1].x) + Math.abs(pair[0].y - pair[1].y);
@@ -21,9 +20,9 @@ const parse = (input: string): SensorInfo[] => {
   const coordinatePairs: CoordPair[] = lines.map((line) =>
     line.split(":").reduce(
       (acc, text, idx) => {
-        const matches = text.match(/[0-9-]+/g) ?? [];
-        if (idx === 0) return [new Coord(matches.join(",")), acc[1]];
-        return [acc[0], new Coord(matches.join(","))];
+        const matches = (text.match(/[0-9-]+/g) ?? []) as number[];
+        if (idx === 0) return [Coord.fromArr(matches), acc[1]];
+        return [acc[0], Coord.fromArr(matches)];
       },
       [new Coord("-1,-1"), new Coord("-1,-1")]
     )
@@ -42,17 +41,17 @@ const findMinAndMaxTouchesOfRow =
     hardMin: number = Number.MIN_SAFE_INTEGER,
     hardMax: number = Number.MAX_SAFE_INTEGER
   ) =>
-  (sensor: SensorInfo): [number, number] => {
+  (sensor: SensorInfo): Coord => {
     const { sensorLoc, mDist } = sensor;
     const { x: sensorX, y: sensorY } = sensorLoc;
 
     const distanceFromRow = Math.abs(yIdx - sensorY);
     const leftOver = mDist - distanceFromRow;
-    if (leftOver < 0) return [0, 0];
+    if (leftOver < 0) return Coord.fromArr([0, 0]);
 
     const min = Math.max(sensorX - leftOver, hardMin);
     const max = Math.min(sensorX + leftOver, hardMax);
-    return [min, max];
+    return Coord.fromArr([min, max]);
   };
 
 const findRowTouches =
@@ -61,22 +60,45 @@ const findRowTouches =
     hardMin: number = Number.MIN_SAFE_INTEGER,
     hardMax: number = Number.MAX_SAFE_INTEGER
   ) =>
-  (sensors: SensorInfo[]): [number, number][] => {
+  (sensors: SensorInfo[]): Coord[] => {
     const minMaxer = findMinAndMaxTouchesOfRow(yIdx, hardMin, hardMax);
-    return sensors.reduce((acc, current) => mergeAll(minMaxer(current), acc), [
-      [0, 0],
-    ] as [number, number][]);
+    return sensors.reduce(
+      (acc, current) => {
+        const minMax = minMaxer(current);
+        return mergeAll(minMax, acc);
+      },
+      [Coord.fromArr([0, 0])]
+    );
   };
+
+const containedCount = (arrs: Coord[]) =>
+  arrs.reduce((acc, current) => acc + (current.y - current.x), 0);
 
 const main = () => {
   const sensors = parse(readInput(__dirname));
 
   // Part 1
-  const y = 2_000_000;
+  const Y = 2_000_000;
 
-  const rowTouches = findRowTouches(y)(sensors);
+  const rowTouches = findRowTouches(Y)(sensors);
   const rowTouchesCount = containedCount(rowTouches);
   logAndAssert(rowTouchesCount, 5_040_643);
+
+  // Part 2
+  console.warn("Part 2 will take over 10 seconds to run");
+  const MIN = 0;
+  const MAX = 4000000;
+
+  for (let i = MAX; i >= MIN; i--) {
+    const rowTouches = findRowTouches(i, MIN, MAX)(sensors);
+    if (rowTouches.length > 1) {
+      const firstTouchLine =
+        rowTouches[0].x < rowTouches[1].x ? rowTouches[0] : rowTouches[1];
+      const tuningFreq = (firstTouchLine.y + 1) * MAX + i;
+      logAndAssert(tuningFreq, 11_016_575_214_126);
+      break;
+    }
+  }
 };
 
 main();
